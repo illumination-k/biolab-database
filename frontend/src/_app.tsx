2e08b5b7bdda48b46269407e9e450f34662d573b
illumination-k <illumination.k.27@gmail.com>
2021-06-08 11:46:58 +0900

    add .env.dev

--- frontend/src/_app.tsx
+++ frontend/src/_app.tsx
@@ -7,22 +7,32 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import theme from "@libs/theme";
 
-const cache = createCache({ key: "css", prepend: true });
-cache.compat = true;
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const materialUiCache = createCache({ key: "css", prepend: true });
+materialUiCache.compat = true;
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  cache,
+});
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   return (
-    <CacheProvider value={cache}>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <ApolloProvider client={client}>
+      <CacheProvider value={materialUiCache}>
+        <Head>
+          <title>My page</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </ApolloProvider>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,6 +6,8 @@ const index = () => {
   return (
     <Layout>
       <h1>test</h1>
+      <p>{process.env.NEXT_PUBLIC_BACKEND_URL}</p>
+      <p>a</p>
     </Layout>
   );
 };
